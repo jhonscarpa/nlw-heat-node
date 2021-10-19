21fffaf48286fec6e9b6566d213097af7a55e92a
jhonscarpa <scarpa.jhonatan@outlook.com>
2021-10-18 22:26:56 -0300

    recuperar token/user

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,44 @@
+import axios from 'axios'
+
+interface IUserResponse {
+  avatar_url: string
+  login: string
+  id: number
+  name: string
+}
+
+interface IAccesTokenResponse {
+  access_token: string
+}
+
+class AuthenticateUserService {
+  async execute(code: string) {
+    const url = 'https://github.com/login/oauth/access_token'
+
+    const { data: accessTokenResponse } = await axios.post<IAccesTokenResponse>(
+      url,
+      null,
+      {
+        params: {
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code,
+        },
+        headers: {
+          Accept: 'application/json',
+        },
+      },
+    )
+    const response = await axios.get<IUserResponse>(
+      'https://api.github.com/user',
+      {
+        headers: {
+          authorization: `Bearer ${accessTokenResponse.access_token}`,
+        },
+      },
+    )
+    return response.data
+  }
+}
+
+export { AuthenticateUserService }
