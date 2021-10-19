465efecd9927441817100029c303dcc5410b749a
jhonscarpa <scarpa.jhonatan@outlook.com>
2021-10-19 20:47:36 -0300

    autenticação do usuário

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import prismaClient from '../prisma'
+import { sign } from 'jsonwebtoken'
 
 interface IUserResponse {
   avatar_url: string
@@ -8,7 +9,7 @@ interface IUserResponse {
   name: string
 }
 
-interface IAccesTokenResponse {
+interface IAccessTokenResponse {
   access_token: string
 }
 
@@ -16,10 +17,8 @@ class AuthenticateUserService {
   async execute(code: string) {
     const url = 'https://github.com/login/oauth/access_token'
 
-    const { data: accessTokenResponse } = await axios.post<IAccesTokenResponse>(
-      url,
-      null,
-      {
+    const { data: accessTokenResponse } =
+      await axios.post<IAccessTokenResponse>(url, null, {
         params: {
           client_id: process.env.GITHUB_CLIENT_ID,
           client_secret: process.env.GITHUB_CLIENT_SECRET,
@@ -28,8 +27,7 @@ class AuthenticateUserService {
         headers: {
           Accept: 'application/json',
         },
-      },
-    )
+      })
     const response = await axios.get<IUserResponse>(
       'https://api.github.com/user',
       {
@@ -56,7 +54,23 @@ class AuthenticateUserService {
         },
       })
     }
-    return response.data
+
+    const token = sign(
+      {
+        user: {
+          name: user.name,
+          avatar_url: user.avatar_url,
+          id: user.id,
+        },
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: '1d',
+      },
+    )
+
+    return { token, user }
   }
 }
 
